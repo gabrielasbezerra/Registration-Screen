b5522c67dfc5acde27cdf04d1cdb4aedf7802913
Gabriela Bezerra <gabrielabezerra2@outlook.com>
2022-08-24 22:59:28 -0300

    melhorias

--- index.js
+++ index.js
@@ -3,32 +3,125 @@ btn.addEventListener("click", function(){
     location.replace("signin.html")
 });
 
-const btnCriar = document.querySelector ("#criar")
-const emailInput = document.querySelector("#email");
-const confEmailInput = document.querySelector("#confEmail");
-const passInput = document.querySelector("#password");
-const confPassInput = document.querySelector("#confPass");
-const nameInput = document.querySelector("#name");
+const btnCriar = document.querySelector ("#criar");
+
+var email = document.querySelector("#email");
+var labelEmail = document.querySelector("#labelEmail");
+var validEmail = false;
+
+var confEmail = document.querySelector("#confEmail");
+var labelConfEmail = document.querySelector("#labelConfEmail");
+var validConfEmail = false;
+
+var senha = document.querySelector("#senha");
+var labelSenha = document.querySelector("#labelSenha");
+var validSenha = false;
+
+var confSenha = document.querySelector("#confSenha");
+var labelConfSenha = document.querySelector("#labelConfSenha");
+var validConfSenha = false;
+
+var nome = document.querySelector("#nome");
+var labelNome = document.querySelector("#labelNome");
+var validNome = false;
+
+var msgError = document.querySelector ("#msgError");
+var msgSucess = document.querySelector ("#msgSucess");
+
+nome.addEventListener('keyup', () => {
+    if (nome.value.length <= 1) {
+        labelNome.setAttribute('style', 'color: red')
+        validNome = false;
+    }
+    else {
+        labelNome.setAttribute('style', 'color: green')
+        validNome = true;
+    }
+})
+
+email.addEventListener('keyup', () => {
+    if (email.value.length <= 5) {
+        labelEmail.setAttribute('style', 'color: red')
+        validEmail = false;
+    }
+    else {
+        labelEmail.setAttribute('style', 'color: green')
+        validEmail = true;
+    }
+})
+
+senha.addEventListener('keyup', () => {
+    if (senha.value.length <= 5) {
+        labelSenha.setAttribute('style', 'color: red')
+        validSenha = false;
+    }
+    else {
+        labelSenha.setAttribute('style', 'color: green')
+        validSenha = true;
+    }
+})
+
+confSenha.addEventListener('keyup', () => {
+    if (senha.value != confSenha.value) {
+        confSenha.setAttribute('style', 'color: red')
+        validConfSenha = false;
+    }
+    else {
+        confSenha.setAttribute('style', 'color: green')
+        validConfSenha = true;
+    }
+})
+
+confEmail.addEventListener('keyup', () => {
+    if (email.value != confEmail.value) {
+        confEmail.setAttribute('style', 'color: red')
+        validConfEmail = false;
+    }
+    else {
+        confEmail.setAttribute('style', 'color: green')
+        validConfEmail = true;
+    }
+})
 
 
-btnCriar.addEventListener('click', function(e){
+const btn2 = document.querySelector("#criar");
+btn2.addEventListener("click", function(e){
     e.preventDefault();
-    const emailValue = document.querySelector("#email").value;
-    const confEmailValue = document.querySelector ("#confEmail").value;
-    const passwordValue = document.querySelector("#password").value;
-    const confPassValue = document.querySelector ("#confPass").value;
-    const nameValue = document.querySelector ("#name").value;
+    if (validConfEmail && validConfSenha && validEmail && validNome && validSenha){
+        var listaUsuario = JSON.parse(localStorage.getItem('listaUsuario') || '[]')
 
-    if (emailValue === confEmailValue) {
-        if (passwordValue === confPassValue){
-            if (nameValue != ''){ window.open("welcome.html")
+        listaUsuario.push(
+            {
+                nomeCad: nome.value,
+                emailCad: email.value,
+                senhaCad: senha.value,
+    
             }
-            else{alert("Favor preencher todos os campos.")}
-        }
-        else{alert("Confirmação de senha não corresponde a senha cadastrada, digite novamente.")}
-        }
-    else{alert("Confirmação de Email não corresponde ao email cadastrado, digite novamente.")}
+        )
+        localStorage.setItem('listaUsuario', JSON.stringify(listaUsuario))
+
+
+        msgSucess.setAttribute('style', 'display: block')
+        msgSucess.innerHTML = '<strong>Cadastrando usuário...</strong>'
+        msgError.innerHTML = ''
+        msgError.setAttribute ('style', 'display: none')
+
+        setTimeout(() => {
+            window.location.href = 'signin.html'
+        }, 2000)
+        
+    }
+    else {
+        msgError.setAttribute('style', 'display: block')
+        msgError.innerHTML = '*Preencha todos os campos corretamente.'
+        msgSucess.innerHTML = ''
+        msgSucess.setAttribute ('style', 'display: none')
+    }
+
 });
+
+
+
 
 const btnDark = document.getElementById ("btn");
 
@@ -36,30 +129,3 @@ btnDark.addEventListener("change", (e) => {
     document.body.classList.toggle("dark", e.target.checked);
 })
 
-
-
-/* 
-    function cadastro (){
-    var name = document.getElementById ("name");
-    var email = document.getElementById ("email");
-    var confEmail = document.getElementById ("confEmail");
-    var password = document.getElementById ("password");
-    var confPass = document.getElementById ("confPass");
-
-    var dados = localStorage.getItem("dadosCadastro")
-    
-    if (dados == null){
-        localStorage.setItem("dadosCadastro", "[]");
-        dados = [];
-    }
-    var auxRegistro = {
-        emailValue: email.value,
-        confEmailValue : confEmail.value,
-        passwordValue : password.value,
-        confPassValue : confPass.value,
-        nameValue : nameValue.value   
-    }
-    dados.push(auxRegistro);
-    localStorage.setItem("dadosCadastro", JSON.stringify(dados));
-
-}*/
--- signin.js
+++ signin.js
@@ -3,30 +3,49 @@ btn.addEventListener("click", function(){
     location.replace("index.html")
 });
 
-const emailDB = "gabriela@gmail.com";
-const passDB = "12345";
-
-const btn2 = document.querySelector("#entrar");
-btn2.addEventListener("click", function(e){
-    e.preventDefault();
-
-    const password = document.querySelector("#password").value;
-    const email = document.querySelector("#email").value;
-    console.log(password);
-    console.log(email);
-
-    if (email == emailDB) {
-        if (password == passDB) {
-            window.open("welcome.html")
-        }
-            else {alert ("Senha Inválida")
-        }
-    }
-    else (alert("Usuário Inexistente"))
-});
-
 const btnDark = document.getElementById ("btn");
 
 btnDark.addEventListener("change", (e) => {
     document.body.classList.toggle("dark", e.target.checked);
 })
+
+const btn2 = document.querySelector("#entrar");
+btn2.addEventListener("click", function(e){
+    e.preventDefault();
+    
+    var email = document.querySelector ("#email");
+    var labelEmail = document.querySelector ("#labelEmail")
+
+    var senha = document.querySelector ("#senha")
+    var labelSenha = document.querySelector ("#labelSenha")
+
+    var msgError = document.querySelector ("#msgError")
+    var listaUsuario = []
+
+    var userValid = {
+        nome: '',
+        email: '',
+        senha: ''
+    }
+
+    listaUsuario = JSON.parse(localStorage.getItem('listaUsuario'))
+    
+    listaUsuario.forEach((item) =>{
+        if (email.value == item.emailCad && senha.value == item.senhaCad){
+            userValid = {
+                nome: item.nomeCad,
+                email: item.emailCad,
+                senha: item.senhaCad
+            }
+        }
+    })
+    if (email.value == userValid.email && senha.value == userValid.senha) {
+        window.location.href = 'welcome.html'
+    }else {
+        labelEmail.setAttribute ('style', 'color: red')
+        labelSenha.setAttribute ('style', 'color: red')
+        msgError.setAttribute('style', 'display:block')
+        msgError.innerHTML ='*Usuário ou senha incorretos'
+        email.focus()
+    }
+});
